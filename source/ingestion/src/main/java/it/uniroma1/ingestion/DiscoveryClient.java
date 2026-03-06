aef3f6ffc6b3dd2e12798a77f424b5fba16813e4
gaiaaaaaaa <rossi.1933502@studenti.uniroma1.it>
2026-03-06 17:47:42 +0100

    Sensors and Pubsub discovery
    Sensors and Pubsub discovery

---
+++ source/ingestion/src/main/java/it/uniroma1/ingestion/DiscoveryClient.java
@@ -0,0 +1,40 @@
+package it.uniroma1.ingestion;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class DiscoveryClient {
+    private static final String BASE_URL = "http://localhost:8080/api/discovery";
+
+    @GetMapping("/discovery/rest_sensors")
+    public List<RestSensor> getDiscoveryRest_sensors() throws Exception {
+        
+        ObjectMapper mapper = new ObjectMapper();
+        URL url = new URL(BASE_URL);
+        
+        try(InputStream input = url.openStream()){
+        
+            DiscoveryResponse dr = mapper.readValue(input, DiscoveryResponse.class);
+
+            return dr.getRest_sensors();
+        }
+    }
+    
+    @GetMapping("/discovery/pubsub_streams")
+    public List<PubsubStream> getDiscoveryPubsub_streams() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        URL url = new URL(BASE_URL);
+
+        try(InputStream input = url.openStream()){
+            DiscoveryResponse dr = mapper.readValue(input, DiscoveryResponse.class);
+            return dr.getPubsub_streams();
+        }
+    }
+}
---
+++ source/ingestion/src/main/java/it/uniroma1/ingestion/DiscoveryResponse.java
@@ -0,0 +1,44 @@
+package it.uniroma1.ingestion;
+
+import java.util.List;
+
+public class DiscoveryResponse {
+
+    private String schema_version;
+    private String schema_policy;
+    private List<RestSensor> rest_sensors;
+    private List<PubsubStream> pubsub_streams;
+
+    public String getSchema_version() { 
+        return schema_version; 
+    }
+    
+    public void setSchema_version(String schema_version) { 
+        this.schema_version = schema_version; 
+    }
+
+    public String getSchema_policy() { 
+        return schema_policy; 
+    }
+    
+    public void setSchema_policy(String schema_policy) { 
+        this.schema_policy = schema_policy;
+    }
+
+    public List<RestSensor> getRest_sensors() { 
+        return rest_sensors;
+    }
+    
+    public void setRest_sensors(List<RestSensor> rest_sensors) { 
+        this.rest_sensors = rest_sensors;
+    }
+
+    public List<PubsubStream> getPubsub_streams() { 
+        return pubsub_streams;
+    }
+    
+    public void setPubsub_streams(List<PubsubStream> pubsub_streams) { 
+        this.pubsub_streams = pubsub_streams; 
+    }
+
+}
